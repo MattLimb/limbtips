1863750bcc4f757c01867d33ac662a4dc71d6ff7
Matt Limb <matt.limb17@gmail.com>
2020-10-18 21:27:25 +0100

    update to use waitress

--- limbtips/__main__.py
+++ limbtips/__main__.py
@@ -1,6 +1,8 @@
 from limbtips import create_app, config
+import waitress
 
 app = create_app(debug=config["server"]["debug"])
 
 if __name__ == "__main__":
-    app.run(host=config["server"]["host"], port=config["server"]["port"])
+    #app.run(host=config["server"]["host"], port=config["server"]["port"])  
+    waitress.serve(app, host=config["server"]["host"], port=config["server"]["port"])
